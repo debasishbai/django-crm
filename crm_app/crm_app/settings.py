9b6743732b7b68f699acf8ffd89ef0598375cc70
debasishbai <bai.debasish123@gmail.com>
2017-02-01 13:22:45 +0530

    created the marketing app

--- crm_app/crm_app/settings.py
+++ crm_app/crm_app/settings.py
@@ -53,6 +53,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crmapp.marketing'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
